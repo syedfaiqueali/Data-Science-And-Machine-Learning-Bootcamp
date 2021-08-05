0f2c80681dfc848dcf864d0efdbeb4432cbe1220
Faiq Ali <faiqueali017@gmail.com>
2021-08-05 19:44:37 +0500

    Styling an HTML Canvas

---
+++ 10. Serving a Tensorflow Model through a website/math_garden_stub/drawing.js
@@ -0,0 +1,52 @@
+// CONSTANTS
+const BACKGROUND_COLOR = '#000000';
+const LINE_COLOR = '#FFFFFF'
+const LINE_WIDTH = 15;
+
+// VARIABLES
+var currentX = 0;
+var currentY = 0;
+var previousX = 0;
+var previousY = 0;
+
+// FUNCTIONS
+
+function prepareCanvas() {
+  console.log('Preparing canvas');
+  const canvas = document.getElementById('my-canvas')
+  var context = canvas.getContext('2d') //2D canvas
+
+  context.fillStyle = BACKGROUND_COLOR;
+  context.fillRect(0, 0, canvas.width, canvas.height); //(x,y,width,height)
+
+  //Set stroke size and color
+  context.strokeStyle = LINE_COLOR;
+  context.lineWidth = LINE_WIDTH;
+  context.lineJoin = 'round'  //For Smoothness of line
+
+  //click anywhere on screen will trigger this event
+  document.addEventListener('mousedown', function(event){
+    console.log('X:'+event.clientX);
+    console.log('Y:'+event.clientY);
+  });
+
+  //for cursor movement
+  document.addEventListener('mousemove', function(event){
+    //Setting last X value
+    previousX = currentX;
+    //Setting canvas start(Top-Left) to x=0
+    currentX = event.clientX - canvas.offsetLeft;  // where mouse's x is currently
+
+    //Setting last y value
+    previousY = currentY;
+    //Setting canvas start(Top-Left) to y=0
+    currentY = event.clientY - canvas.offsetTop;  // where mouse's y is currently
+
+    context.beginPath();  //Create list of coordinates
+    context.moveTo(previousX, previousY);     //Set starting point of the path
+    context.lineTo(currentX, currentY);    //Set ending points of the path to draw line
+    context.closePath();
+    context.stroke();
+  });
+
+}
